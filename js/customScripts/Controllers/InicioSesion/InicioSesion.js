cc41dc01de4ec7a3819c4deb8e66151f7a584572
Alejandro Brizo <abrizho_8594@hotmail.com>
2018-04-22 05:29:19 -0600

    Incorporación básica de Angular JS

---
+++ js/customScripts/Controllers/InicioSesion/InicioSesion.js
@@ -0,0 +1,8 @@
+'use strict'
+
+angular.module('SoundskapeApp')
+
+	.controller('InicioSesionController', ['$scope','$location','$http', function($scope, $location, $http){
+	 
+
+	}]);
---
+++ js/customScripts/Controllers/InicioSesion/Registrarse.js
@@ -0,0 +1,8 @@
+'use strict'
+
+angular.module('SoundskapeApp')
+
+	.controller('RegistrarseController', ['$scope','$location','$http', function($scope, $location, $http){
+	 
+
+	}]);
