a346a4ab0579310b1ea63dd4cf43d256bfa72e1c
Beyson <38594100+Beyson@users.noreply.github.com>
2018-04-27 18:57:36 -0600

    Add files via upload

--- js/customScripts/Controllers/InicioSesion/InicioSesion.js
+++ js/customScripts/Controllers/InicioSesion/InicioSesion.js
@@ -3,6 +3,19 @@
 angular.module('SoundskapeApp')
 
 	.controller('InicioSesionController', ['$scope','$location','$http', function($scope, $location, $http){
-	 
+	 	
+	 	$scope.IniciarUsuario = function(){
+	 		
+	 	$http.post("php/dbverificarusuario.php", {
+		                                'namesurname':$scope.namesurname,
+		                                'password':$scope.password		                                	                                
+		                }).then(function successCallback(response) {  
+		           
+						console.log('Send data sucessfully');		              
 
-	}]);
+		         
+		         });
+			
+	 	}
+
+	 }]);
--- js/customScripts/Controllers/InicioSesion/Registrarse.js
+++ js/customScripts/Controllers/InicioSesion/Registrarse.js
@@ -1,8 +1,24 @@
 'use strict'
 
 angular.module('SoundskapeApp')
-
+	
 	.controller('RegistrarseController', ['$scope','$location','$http', function($scope, $location, $http){
-	 
+	 				              
 
+			$scope.insertuser=function(){
+				
+
+				if(($scope.password==$scope.confirm)&&$scope.terms){
+					 	$http.post("php/dbinsertarusuario.php", {
+		                                'namesurname':$scope.namesurname,
+		                                'email':$scope.email,
+		                                'password':$scope.password		                                	                                
+		                }).then(function successCallback(response) {  
+		           
+						console.log('Send data sucessfully');		              
+
+		         
+		         });
+		          }else{ if(!$scope.terms){$scope.messageconfirm='Verificar terminos y condiciones';}else{$scope.messageconfirm='Confirmar Contraseña';}}
+          }
 	}]);
