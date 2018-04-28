f1003928171de70c92254266b30125203867c1a3
Beyson <38594100+Beyson@users.noreply.github.com>
2018-04-27 18:56:33 -0600

    Add files via upload

---
+++ php/dbinsertarusuario.php
@@ -0,0 +1,27 @@
+<?php 
+$data = json_decode(file_get_contents("php://input"));
+
+
+$namesurname = $data->namesurname;
+$email = $data->email;
+$password = $data->password;
+
+
+
+$con = mysql_connect("localhost","root","");
+mysql_select_db("database_name");
+
+
+
+
+$sql = "Script sql goes here";
+
+$result = mysql_query($sql);
+
+
+
+$con->close();
+
+
+
+?>
